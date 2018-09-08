521bd4832fb078eb956b543ea0b9e68693d8d1a1
n3m3z3s <sergiombaldez@gmail.com>
2018-09-08 10:09:45 -0300

    TDE ES

---
+++ calc.py
@@ -0,0 +1,20 @@
+def soma(a,b):
+    try:
+     return float(a) + float(b)
+    except:
+     return None
+def sub(a,b):
+    try:
+     return float(a) - float(b)
+    except:
+     return None
+def multi(a,b):
+    try:
+     return float(a) * float(b)
+    except:
+     return None
+def div(a,b):
+    try:
+     return float(a) / float(b)
+    except:
+     return None
